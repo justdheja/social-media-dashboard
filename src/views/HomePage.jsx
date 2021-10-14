29cd8bbcf8869846271fd76d8c4953b4b1baf7c9
justdheja <justdheja@gmail.com>
2021-10-14 22:10:20 +0700

    feat: create pages

---
+++ src/views/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">Home</h2>
+    </div>
+  );
+}
+ 
+export default HomePage;
---
+++ src/views/PostList.jsx
@@ -0,0 +1,9 @@
+const PostList = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">Post list</h2>
+    </div>
+  );
+}
+ 
+export default PostList;
